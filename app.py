49fbf437c85e5faab63b474f9e6e8ec73e7e2cec
Crissky <pg2006pe@hotmail.com>
2026-05-10 14:47:52 -0300

    Corrige configuração do logging

--- app.py
+++ app.py
@@ -11,38 +11,46 @@ IS_PRODUCTION = config("IS_PRODUCTION", cast=bool, default=True)
 
 
 # SET LOGGING ================================================================
-logger = logging.getLogger(__name__)
 if IS_PRODUCTION:
-    logger.setLevel(logging.INFO)
+    level = logging.INFO
 else:
-    logger.setLevel(logging.DEBUG)
+    level = logging.DEBUG
 
-file_handler = logging.FileHandler("teikoku.log", mode="w")
-console_handler = logging.StreamHandler()
 formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt='%Y-%m-%d %H:%M:%S',
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
+
+file_handler = logging.FileHandler("teikoku.log", mode="w", encoding="utf-8")
+console_handler = logging.StreamHandler()
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
-file_handler.stream.reconfigure(encoding='utf-8')
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(level)
+
+root_logger.addHandler(file_handler)
+root_logger.addHandler(console_handler)
 # SET LOGGING ================================================================
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
     """Run the bot."""
+
+    logger.info("Iniciando Teikoku no Densetshu...")
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TELEGRAM_TOKEN).build()
 
+    logger.info("Adicionando handlers.")
     # Add Single Handler =====================================================
     application.add_handler(CLOSE_MSG_HANDLER)
 
     # Add Multiple Handlers ==================================================
     application.add_handlers(SIGNUP_HANDLERS)
 
-
+    logger.info("Iniciando run_polling() Teikoku no Densetshu!")
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
 
