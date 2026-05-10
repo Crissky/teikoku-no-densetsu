948e2478223e7da08c2641d46a236bfdd756d6e5
Crissky <pg2006pe@hotmail.com>
2026-05-10 17:12:48 -0300

    Corrige formatação do código

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,2 +1,2 @@
-from bot.conversations.general.close import CLOSE_MSG_HANDLER
-from bot.conversations.user.signup import SIGNUP_HANDLERS
+from bot.conversations.general.close import CLOSE_MSG_HANDLER  # noqa
+from bot.conversations.user.signup import SIGNUP_HANDLERS  # noqa
