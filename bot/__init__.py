f601eda7f73665d9bfc5d6cccfae8df14bb34a85
Crissky <pg2006pe@hotmail.com>
2026-05-10 17:43:40 -0300

    Refatora user para register

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,2 +1,2 @@
 from bot.conversations.general.close import CLOSE_MSG_HANDLER  # noqa
-from bot.conversations.user.signup import SIGNUP_HANDLERS  # noqa
+from bot.conversations.register.signup import SIGNUP_HANDLERS  # noqa
