80345580369bcd95041cf310f0709e36925ed611
Crissky <pg2006pe@hotmail.com>
2026-05-13 20:11:30 -0300

    Adiciona FAIL_UPDATE_NOT_ARGS

---
+++ bot/constants/messages.py
@@ -0,0 +1,7 @@
+FAIL_UPDATE_NOT_ARGS = (
+    "Após o comando passe o atributo que deseja alterar e o novo "
+    "valor unidos pelo sinal de igual (sem espaço entre eles).\n"
+    "Exemplo: `silent`=`true`.\n"
+    "Múltiplos atributos podem ser passados separados por vírgula.\n"
+    "Exemplo: `arg_1`=`true`, `arg_2`=`123`, `arg_3`=`abc`.\n\n"
+)
