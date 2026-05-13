1f9e6b975ba1884dad9bff52927f91d4fc7c97ea
Crissky <pg2006pe@hotmail.com>
2026-05-13 20:11:15 -0300

    Adiciona comandos de atualiza player e group

--- bot/constants/commands.py
+++ bot/constants/commands.py
@@ -1,3 +1,7 @@
 PLAYER_COMMNADS = ["player", "jogador"]
 SIGNUP_COMMNADS = ["signup", "cadastrar"]
 SIGNUP_GROUP_COMMNADS = ["signup_group", "cadastrar_group"]
+
+# UPDATE COMMANDS
+UPDATE_GROUP_COMMNADS = ["update_group", "atualizar_grupo"]
+UPDATE_PLAYER_COMMNADS = ["update_player", "atualizar_jogador"]
