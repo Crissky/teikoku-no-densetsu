79eb09f1981f1579f747013dd47c417207b6ce27
Crissky <pg2006pe@hotmail.com>
2026-05-14 03:50:26 -0300

    Adiciona player messages

--- bot/constants/message.py
+++ bot/constants/message.py
@@ -1,14 +1,26 @@
+# PLAYER
+PLAYER_ALREADY_REGISTERED_FORMAT = (
+    "*Player* com *USER ID*: *{id}* já está cadastrado."
+)
+REGISTERED_PLAYER_FORMAT = (
+    "Olá {name}!\n"
+    "Você foi cadastrado com sucesso!\n\n"
+    "{subsection}"
+    "{telegram_text}"
+)
 FAIL_UPDATE_NOT_ARGS = (
     "⚠️ Este comando requer *argumentos* para funcionar.\n\n"
-
     "*Como usar:*\n"
     "Informe o atributo e o novo valor separados por `=`.\n"
     "Exemplo: `atributo`=`valor`\n\n"
-
     "*Múltiplos atributos:*\n"
     "Separe cada par por espaço.\n"
     "Exemplo: `arg_1`=`true` `arg_2`=`123` `arg_3`=`abc`\n\n"
-
     "⚠️ Argumentos fora desse formato, atributos não alteráveis ou valores "
-    "inválidos serão ignorados.\n"
+    "inválidos serão ignorados.\n\n"
+)
+PLAYER_ALTERABLE_ATTRIBUTES_HEADER = "*Atributos alteráveis do jogador*:\n"
+NO_CHANGE_IN_PLAYER = (
+    "*Nenhuma alteração* foi feito no jogador. "
+    "Nenhum *atributo* ou *valor* válido foi informado."
 )
