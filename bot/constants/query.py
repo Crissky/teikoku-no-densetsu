fb871a2d6c79ed5aae8c9f31b0c3942b19e53ecb
Crissky <pg2006pe@hotmail.com>
2026-05-15 01:35:38 -0300

    Adiciona refresh e update para group

--- bot/constants/query.py
+++ bot/constants/query.py
@@ -1,8 +1,14 @@
 import re
 
-# CALLBACK COMMANDS
+# CALLBACK COMMANDS ==========================================================
 CALLBACK_COMMAND_CLOSE = "$close"
+
+# PLAYER
 CALLBACK_COMMAND_REFRESH_PLAYER = "$refresh_player"
 CALLBACK_COMMAND_UPDATE_PLAYER = "$update_player"
+
+# GROUP
+CALLBACK_COMMAND_REFRESH_GROUP = "$refresh_group"
+CALLBACK_COMMAND_UPDATE_GROUP = "$update_group"
 
 ESCAPED_CALLBACK_COMMAND_CLOSE = re.escape(CALLBACK_COMMAND_CLOSE)
