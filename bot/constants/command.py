80433893195ec11906d1560f58e1665d7ee29473
Crissky <pg2006pe@hotmail.com>
2026-05-16 01:15:58 -0300

    Adiciona e refatora comandos

--- bot/constants/command.py
+++ bot/constants/command.py
@@ -1,9 +1,15 @@
-PLAYER_COMMNADS = ["player", "jogador"]
+# REGISTER COMMANDS
 SIGNUP_COMMNADS = ["signup", "cadastrar"]
-
-GROUP_COMMNADS = ["group", "grupo"]
 SIGNUP_GROUP_COMMNADS = ["signup_group", "cadastrar_group"]
 
+# SHOW COMMANDS
+GROUP_COMMNADS = ["group", "grupo"]
+PLAYER_COMMNADS = ["player", "jogador"]
+
 # UPDATE COMMANDS
-UPDATE_GROUP_COMMNADS = ["update_group", "atualizar_grupo"]
-UPDATE_PLAYER_COMMNADS = ["update_player", "atualizar_jogador"]
+SET_ATTR_GROUP_COMMNADS = ["update_group", "set_attr_group", "atualizar_grupo"]
+SET_ATTR_PLAYER_COMMNADS = [
+    "update_player",
+    "set_attr_player",
+    "atualizar_jogador",
+]
