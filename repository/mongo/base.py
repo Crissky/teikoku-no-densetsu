56021346b9a54a58cafbeb8c6b3cc30a0379b9fe
Crissky <pg2006pe@hotmail.com>
2026-05-20 03:01:07 -0300

    Refatora to_dict e adiciona persisted_fields to_dict passa a tratar caso que Model vai popular um campo

--- repository/mongo/base.py
+++ repository/mongo/base.py
@@ -3,7 +3,7 @@ import logging
 from abc import ABC, abstractmethod
 from enum import Enum
 
-from dataclasses import asdict, dataclass, field, fields
+from dataclasses import dataclass, field, fields
 from datetime import datetime
 from typing import Any, ClassVar, Tuple, Union
 
@@ -17,6 +17,7 @@ class MongoBase(ABC):
     _id: Union[ObjectId, str] = field(default_factory=ObjectId)
     created_at: datetime = None
     updated_at: datetime = None
+
     UPDATABLE_ATTR_LIST: ClassVar[Tuple[str]]
 
     def __post_init__(self):
@@ -56,21 +57,26 @@ class MongoBase(ABC):
             )
 
     def to_dict(self) -> dict:
-        data = asdict(self)
         d = {}
         for f in fields(self):
             if not f.init:
                 continue
 
             obj = getattr(self, f.name)
-            if isinstance(obj, MongoBase):
-                d[f.name] = obj._id
-            elif isinstance(obj, Enum):
-                d[f.name] = obj.name
-            else:
-                data[f.name]
+            d[f.name] = self._parse_to_mongo(obj)
 
         return d
+
+    def _parse_to_mongo(self, obj: Any) -> Any:
+
+        if isinstance(obj, list):
+            return [self._parse_to_mongo(o) for o in obj]
+        elif isinstance(obj, MongoBase):
+            return obj.persisted_fields
+        elif isinstance(obj, Enum):
+            return obj.name
+        else:
+            return obj
 
     def translate(self, value: Any) -> Union[str, Any]:
         if value is True:
@@ -86,3 +92,17 @@ class MongoBase(ABC):
     @property
     @abstractmethod
     def telegram_text(self) -> str: ...
+
+    @property
+    def persisted_fields(self) -> Union[dict, ObjectId]:
+        """Representação da classe quando ela é um atributo de outra classe
+        MongoBase.
+
+        Usar self._id se o atributo for instanciado pelo Model, ou seja,
+        se o atributo é um documento de uma collection do MongoDB.
+
+        Usar self.to_dict() se o atributo for instanciado pela classe,
+        ou seja, se o atributo for um campo Object no MongoDB.
+        """
+
+        return self._id
