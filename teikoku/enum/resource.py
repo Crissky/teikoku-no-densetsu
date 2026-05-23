0e249a2b74a5a08fc4193018a7d3d141a377f967
Crissky <pg2006pe@hotmail.com>
2026-05-23 01:50:20 -0300

    Refatora ResourceEnum em duas classes Adiciona CollectibleResourceEnum Adiciona SocialResourceEnum

--- teikoku/enum/resource.py
+++ teikoku/enum/resource.py
@@ -1,19 +1,34 @@
 from enum import Enum
 
 
-class ResourceEnum(Enum):
-    CULTURE = "cultura"
-    FAITH = "fé"
+class CollectibleResourceEnum(Enum):
     FOOD = "alimento"
     METAL = "metal"
     PRECIOUS_METAL = "metal precioso"
-    SCIENCE = "ciência"
     STONE = "rocha"
     WOOD = "madeira"
 
 
+class SocialResourceEnum(Enum):
+    CULTURE = "cultura"
+    FAITH = "fé"
+    SCIENCE = "ciência"
+
+
+ResourceEnum = Enum(
+    "ResourceEnum",
+    {**CollectibleResourceEnum.__members__, **SocialResourceEnum.__members__},
+)
+
+
 class LocationResourceTypeEnum(Enum):
     MINE = "mina"
-    DEPOSIT = "JAZIDA"
+    DEPOSIT = "jazida"
     FOREST = "floresta"
     TEMPLE = "templo"
+
+
+print(CollectibleResourceEnum, list(CollectibleResourceEnum), end="\n\n")
+print(SocialResourceEnum, list(SocialResourceEnum), end="\n\n")
+print(ResourceEnum, list(ResourceEnum), end="\n\n")
+print(type(ResourceEnum.METAL), end="\n\n")
