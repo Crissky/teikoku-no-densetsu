38004b36c3b5d2b13870aa38abba797e7fce5dbf
Crissky <pg2006pe@hotmail.com>
2026-05-25 20:42:05 -0300

    Adiciona novos dados Adiciona TERRAIN_COLORS, TERRAIN_NAMES e DEFAULT_SEED

--- teikoku/data/world.py
+++ teikoku/data/world.py
@@ -1,5 +1,21 @@
-TERRAIN_PROBABILITIES = {
-    0: 75,  # Grama (75% de chance)
-    1: 15,  # Água (15% de chance)
-    2: 10,  # Montanha (10% de chance)
+TERRAIN_COLORS = {
+    0: (76, 175, 80),  # Grama (Verde padrão)
+    1: (13, 71, 161),  # Água Profunda / Oceano (Azul Escuro)
+    2: (33, 150, 243),  # Água Rasa (Azul claro)
+    3: (203, 189, 147),  # Areia / Praia (Bege)
+    4: (27, 94, 32),  # Pântano / Floresta (Verde Escuro)
+    5: (139, 195, 74),  # Colinas (Verde Oliva)
+    6: (158, 158, 158),  # Montanha (Cinza)
+    7: (245, 245, 245),  # Pico Nevado (Branco)
 }
+TERRAIN_NAMES = {
+    0: "Grama",
+    1: "Mar Profundo",
+    2: "Água Rasa",
+    3: "Areia / Praia",
+    4: "Pântano / Floresta",
+    5: "Colinas",
+    6: "Montanha",
+    7: "Pico Nevado",
+}
+DEFAULT_SEED = 42
