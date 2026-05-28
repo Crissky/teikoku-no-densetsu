c864fa7a39e3a05ed473f7b25ef326d6b1ece7d8
Crissky <pg2006pe@hotmail.com>
2026-05-28 00:15:47 -0300

    Refatora world e adiciona mapa parcial

--- teikoku/entity/world/world.py
+++ teikoku/entity/world/world.py
@@ -1,13 +1,21 @@
-from dataclasses import dataclass, field
 import logging
-import random
-from typing import Dict, List, Optional, Tuple
+
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Tuple
+
+import noise
 
 from repository.mongo.base import MongoBase
-from teikoku.data.world import TERRAIN_PROBABILITIES
+from teikoku.data.world import (
+    DEFAULT_WORLD_SEED,
+    DEFAULT_TERRAIN_SIZE,
+    PNOISE2_CONFIG,
+    PNOISE2_SCALE,
+)
 from teikoku.entity.unit.unit_base import UnitBase
 from teikoku.entity.world.city import City
 from teikoku.entity.world.coor import Coordinate
+from teikoku.enum.terrain import TerrainEnum
 
 logger = logging.getLogger(__name__)
 
@@ -15,36 +23,61 @@ logger = logging.getLogger(__name__)
 @dataclass
 class World(MongoBase):
     name: str
-    base_size: int = 500
     cities: Dict[Tuple[int, int], City] = field(default_factory=dict)
     units: Dict[Tuple[int, int], UnitBase] = field(default_factory=dict)
-    terrain_map: List[List[int]] = field(default_factory=list)
 
     UPDATABLE_ATTR_LIST = ()
 
-    def __post_init__(self):
+    def generate_partial_terrain_map(
+        self,
+        coor: Coordinate = Coordinate(0, 0),
+        size: int = DEFAULT_TERRAIN_SIZE,
+        scale: float = PNOISE2_SCALE,
+        seed: int = DEFAULT_WORLD_SEED,
+    ):
+        half_size1 = size // 2
+        half_size2 = half_size1 if size % 2 == 0 else half_size1 + 1
+        x1 = coor.x - half_size1
+        x2 = coor.x + half_size2
+        y1 = coor.y - half_size1
+        y2 = coor.y + half_size2
+        terrain_map = []
+        print(f"x1: {x1}, x2: {x2}, y1: {y1}, y2: {y2}")
+        for y in range(y1, y2):
+            terrain_map.append([])
+            for x in range(x1, x2):
+                value = noise.pnoise2(
+                    x * scale + seed,
+                    y * scale + seed,
+                    base=seed,
+                    **PNOISE2_CONFIG,
+                )
 
-        if self.base_size <= 0:
-            raise TypeError(
-                "base_size precisa ser um inteiro maior que zero "
-                f"({self.base_size})."
-            )
+                # --- ÁREA DA ÁGUA (Abaixo de -0.12) ---
+                terrain = TerrainEnum.GRASSLAND.value
+                if value < -0.32:
+                    terrain = TerrainEnum.DEEP_SEA.value
+                elif value < -0.2:
+                    terrain = TerrainEnum.SHALLOW_WATER.value
+                elif value < -0.12:
+                    terrain = TerrainEnum.BEACH.value
 
-        if not self.terrain_map:
-            self.terrain_map = self.create_terrain_map()
+                # --- ÁREA DA TERRA PLANA E BIOMAS (Entre -0.12 e 0.25) ---
+                elif value < -0.05:
+                    terrain = TerrainEnum.SWAMP_FOREST.value
+                elif value < 0.18:
+                    terrain = TerrainEnum.GRASSLAND.value
+                elif value < 0.25:
+                    terrain = TerrainEnum.HILLS.value
 
-        super().__post_init__()
+                # --- ÁREA DAS ALTITUDES (Acima de 0.25) ---
+                elif value < 0.36:
+                    terrain = TerrainEnum.MOUNTAIN.value
+                else:
+                    terrain = TerrainEnum.SNOW_PEAK.value
+                terrain_map[-1].append(terrain)
 
-    def generate_terrain_map(self, seed: int = 42):
-        choices = []
-        local_rng = random.Random(seed)
-        for terrain_id, weight in TERRAIN_PROBABILITIES.items():
-            choices.extend([terrain_id] * weight)
-
-        return [
-            [local_rng.choice(choices) for _ in range(self.base_size)]
-            for _ in range(self.base_size)
-        ]
+        return terrain_map
 
     def add_city(self, city: City):
         if not isinstance(city, City):
@@ -86,11 +119,6 @@ class World(MongoBase):
         return self.cities.get(coor, None)
 
     @property
-    def size(self) -> int:
-        total_cities = max(self.total_cities, 1)
-        return self.base_size * total_cities
-
-    @property
     def total_cities(self) -> int:
         return len(self.cities)
 
@@ -98,7 +126,6 @@ class World(MongoBase):
     def telegram_text(self):
         text = f"*Mundo*: {self.name}\n"
         text += f"*Total de Cidades*: {self.total_cities}\n"
-        text += f"*Tamanho*: {self.size} unidades\n"
 
         return text
 
@@ -120,5 +147,8 @@ if __name__ == "__main__":
 
     print("\nWORLD.TO_DICT")
     print(world.to_dict())
+
+    print("\nWORLD.GENERATE_PARTIAL_TERRAIN_MAP")
+    print(world.generate_partial_terrain_map(size=10))
 
     print(" END LOCAL TEST ".center(79, "="))
