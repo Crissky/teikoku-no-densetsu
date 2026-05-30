e82edb5e6c6b66c6ddf5fa12d18293c24a14ea67
Crissky <pg2006pe@hotmail.com>
2026-05-30 00:17:31 -0300

    Adiciona e refatora world data

--- teikoku/data/world.py
+++ teikoku/data/world.py
@@ -1,8 +1,8 @@
 from types import MappingProxyType
 
+# TERRAIN MAP
 DEFAULT_TERRAIN_SIZE = 512
 DEFAULT_WORLD_SEED = 42
-MIN_MAP_SIZE = (1024, 1024)
 PNOISE2_SCALE = 0.005
 PNOISE2_CONFIG = MappingProxyType(
     dict(
@@ -13,3 +13,16 @@ PNOISE2_CONFIG = MappingProxyType(
         repeaty=1024,
     )
 )
+
+# IMAGES =====================================================================
+# BASE MAP
+MIN_MAP_SIZE = (1024, 1024)
+
+# LEGEND MAP
+LEGEND_WORLD_FONT_PATH = "teikoku\assets\fonts\retro_gaming.ttf"
+LEGEND_TITLE_SIZE = 16
+LEGEND_TEXT_SIZE = 14
+LEGEND_BG_COLOR = (30, 30, 30)
+LEGEND_TITLE_COLOR = (255, 255, 255)
+LEGEND_TEXT_COLOR = (230, 230, 230)
+LEGEND_RECT_OUTLINE = (255, 255, 255)
