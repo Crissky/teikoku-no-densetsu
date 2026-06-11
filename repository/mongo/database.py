931466832c8a4ab35726003935709387e671fb66
Crissky <pg2006pe@hotmail.com>
2026-06-10 23:49:20 -0300

    Adiciona tipagem para hint de Collection

--- repository/mongo/database.py
+++ repository/mongo/database.py
@@ -5,6 +5,8 @@ from typing import Any, Union
 from pymongo import MongoClient
 from pymongo.cursor import Cursor
 from pymongo.results import InsertOneResult, UpdateResult, DeleteResult
+from pymongo.synchronous.collection import Collection
+from pymongo.synchronous.database import Database as MongoDatabase
 from decouple import config
 
 URL_CONNECTION = config("URL_CONNECTION")
@@ -20,41 +22,55 @@ class Database:
             url_connection, serverSelectionTimeoutMS=5000
         )
         self.__server_info = self.__client.server_info()
-        self.__database = self.__client[database_name]
+        self.__database: MongoDatabase = self.__client[database_name]
+
+    def get_collection(self, collection) -> Collection:
+        return self.database[collection]
 
     def insert(self, collection: str, data: dict) -> InsertOneResult:
-        return self.database[collection].insert_one(document=data)
+        col = self.get_collection(collection)
+
+        return col.insert_one(document=data)
 
     def find(
         self, collection: str, query: dict, fields: Union[list, dict] = None
     ) -> Union[dict, None]:
-        return self.database[collection].find_one(
-            filter=query, projection=fields
-        )
+        col = self.get_collection(collection)
+
+        return col.find_one(filter=query, projection=fields)
 
     def find_many(
         self, collection: str, query: dict, fields: Union[list, dict] = None
     ) -> Cursor:
-        return self.database[collection].find(filter=query, projection=fields)
+        col = self.get_collection(collection)
+
+        return col.find(filter=query, projection=fields)
 
     def update(self, collection: str, query: dict, data: dict) -> UpdateResult:
-        return self.database[collection].update_one(filter=query, update=data)
+        col = self.get_collection(collection)
+
+        return col.update_one(filter=query, update=data)
 
     def replace(
         self, collection: str, query: dict, data: dict, upsert: bool = True
     ) -> UpdateResult:
-        return self.database[collection].replace_one(
-            filter=query, replacement=data, upsert=upsert
-        )
+        col = self.get_collection(collection)
+
+        return col.replace_one(filter=query, replacement=data, upsert=upsert)
 
     def delete(self, collection: str, query: dict) -> DeleteResult:
-        return self.database[collection].delete_one(filter=query)
+        col = self.get_collection(collection)
+
+        return col.delete_one(filter=query)
 
     def count(self, collection: str, query: dict, **kwargs: Any) -> int:
-        return self.db[collection].count_documents(filter=query, **kwargs)
+        col = self.get_collection(collection)
+
+        return col.count_documents(filter=query, **kwargs)
 
     def length(self, collection: str, query: dict, field: str) -> int:
-        cursor = self.db[collection].aggregate(
+        col = self.get_collection(collection)
+        cursor = col.aggregate(
             [
                 {"$match": query},
                 {"$project": {"length": {"$size": f"${field}"}}},
