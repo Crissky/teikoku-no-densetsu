e89a211b06b3c6df9bd7faa11a8a7728eaba7e5b
Crissky <pg2006pe@hotmail.com>
2026-07-16 00:47:01 -0300

    Adiciona teste local

--- repository/mongo/models/city.py
+++ repository/mongo/models/city.py
@@ -21,3 +21,31 @@ class CityModel(Model):
     populate_fields: dict = property(
         lambda self: {"owner": {PopulateFieldEnum.INITIATOR: PlayerModel}}
     )
+
+
+if __name__ == "__main__":
+    print(" START LOCAL TEST ".center(79, "="))
+    city = City()
+    city_model = CityModel()
+
+    print("COLLECTION NAME:")
+    print(city_model.collection)
+
+    print("\nSAVING CITY...")
+    city_model.save(city)
+
+    print("\nRETRIEVING SAVED CITY...")
+    retrieved_city = city_model.get(123456789)
+    if retrieved_city is None:
+        raise ValueError(f"retrieved_city é None ({retrieved_city}).")
+    print("\nRETRIEVED CITY:")
+    print(retrieved_city)
+    print("\nEQUALS:", city == retrieved_city)
+    if city != retrieved_city:
+        raise ValueError(
+            "O valor salvo é diferente do valor recuperado.\n"
+            f"Valor Salvo: {city}\n"
+            f"Valor Recuperado: {retrieved_city}\n"
+        )
+
+    print(" END LOCAL TEST ".center(79, "="))
