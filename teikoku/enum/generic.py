3b812fb9e8b4133e96b00c04fe452d902cff8f94
Crissky <pg2006pe@hotmail.com>
2026-07-18 12:26:27 -0300

    Adiciona properties de str

--- teikoku/enum/generic.py
+++ teikoku/enum/generic.py
@@ -4,5 +4,17 @@ from enum import Enum
 class GenericEnum(Enum):
 
     @property
-    def lower(self) -> str:
+    def lower_name(self) -> str:
         return self.name.lower()
+
+    @property
+    def upper_name(self) -> str:
+        return self.name.upper()
+
+    @property
+    def lower_value(self) -> str:
+        return str(self.value).lower()
+
+    @property
+    def upper_value(self) -> str:
+        return str(self.value).upper()
