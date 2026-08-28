e3a718bf84788df178f020e0f3c386942502d4d7
Crissky <pg2006pe@hotmail.com>
2026-08-28 00:18:34 -0300

    Adiciona status

--- teikoku/util/reporting/base.py
+++ teikoku/util/reporting/base.py
@@ -1,11 +1,33 @@
-from dataclasses import dataclass
+from teikoku.enum.reporting import ReportingStatusEnum
 
 
 @dataclass
 class ReportingBase:
     message: str
+    status: ReportingStatusEnum
 
     def __init__(self, **kwargs):
+        if "message" not in kwargs:
+            raise ValueError("Reporting precisa do campo 'message'.")
+        if "status" not in kwargs:
+            raise ValueError("Reporting precisa do campo 'status'.")
+
+        message = kwargs["message"]
+        if not isinstance(message, str):
+            raise TypeError(
+                f"O campo 'message' precisa ser do tipo str ({type(message)})."
+            )
+
+        status = kwargs["status"]
+        if not isinstance(status, (str, ReportingStatusEnum)):
+            raise TypeError(
+                "O campo 'status' precisa ser do tipo str ou "
+                f"ReportingStatusEnum ({type(status)})."
+            )
+
+        if isinstance(status, str):
+            kwargs["status"] = ReportingStatusEnum(status)
+
         for name, value in kwargs.items():
             setattr(self, name, value)
         self.kwargs = list(kwargs.keys())
