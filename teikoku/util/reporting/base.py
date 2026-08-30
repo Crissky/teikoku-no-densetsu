cd764e92898aaab2a361c93dd24eff1a65729dc3
Crissky <pg2006pe@hotmail.com>
2026-08-30 01:11:17 -0300

    Adiciona dunder e property

--- teikoku/util/reporting/base.py
+++ teikoku/util/reporting/base.py
@@ -31,3 +31,42 @@ class ReportingBase:
         for name, value in kwargs.items():
             setattr(self, name, value)
         self.kwargs = list(kwargs.keys())
+
+    def __str__(self):
+        return ", ".join(f"{a}={getattr(self, a)}" for a in self.kwargs)
+
+    def __repr__(self):
+        text = str(self)
+        return f"{self.__class__.__name__}({text})"
+
+    @property
+    def report(self) -> dict:
+        return {a: getattr(self, a) for a in self.kwargs}
+
+    @property
+    def text(self) -> str:
+        return self.message
+
+    @property
+    def status_name(self) -> str:
+        return self.status.name
+
+    @property
+    def status_text(self) -> str:
+        return self.status.value
+
+
+if __name__ == "__main__":
+    print(" START LOCAL TEST ".center(79, "="))
+    r = ReportingBase(
+        city="cidade",
+        message="Mensagem de teste.",
+        status="success",
+    )
+    print(r)
+    print(repr(r))
+    print("REPORT:", r.report)
+    print("TEXT:", r.text)
+    print("STATUS NAME:", r.status_name)
+    print("STATUS TEXT:", r.status_text)
+    print(" END LOCAL TEST ".center(79, "="))
