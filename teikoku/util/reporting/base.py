f1200fa0e1fb79d347bc674a188d0640e9b7a239
Crissky <pg2006pe@hotmail.com>
2026-08-31 23:42:24 -0300

    Refatora init para post_init

--- teikoku/util/reporting/base.py
+++ teikoku/util/reporting/base.py
@@ -1,36 +1,36 @@
+from dataclasses import dataclass, fields
+from enum import Enum
+from typing import Iterator, get_type_hints
+
 from teikoku.enum.reporting import ReportingStatusEnum
 
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class ReportingBase:
     message: str
     status: ReportingStatusEnum
 
-    def __init__(self, **kwargs):
-        if "message" not in kwargs:
-            raise ValueError("Reporting precisa do campo 'message'.")
-        if "status" not in kwargs:
-            raise ValueError("Reporting precisa do campo 'status'.")
+    def __post_init__(self):
+        type_hints = get_type_hints(type(self))
 
-        message = kwargs["message"]
-        if not isinstance(message, str):
-            raise TypeError(
-                f"O campo 'message' precisa ser do tipo str ({type(message)})."
-            )
+        # INSTANCIA ENUMS ATRAVÉS DE SEU NOME (STRING)
+        for field in fields(self):
+            if not field.init:
+                continue
 
-        status = kwargs["status"]
-        if not isinstance(status, (str, ReportingStatusEnum)):
-            raise TypeError(
-                "O campo 'status' precisa ser do tipo str ou "
-                f"ReportingStatusEnum ({type(status)})."
-            )
+            field_type = type_hints.get(field.name)
+            if (
+                isinstance(field_type, type)
+                and issubclass(field_type, Enum)
+                and isinstance(getattr(self, field.name), str)
+            ):
+                object.__setattr__(
+                    self,
+                    field.name,
+                    field_type(getattr(self, field.name)),
+                )
 
-        if isinstance(status, str):
-            kwargs["status"] = ReportingStatusEnum(status)
-
-        for name, value in kwargs.items():
-            setattr(self, name, value)
-        self.kwargs = list(kwargs.keys())
+        # self._check_init_types() # TODO copiar e adaptar do `MongoBase`
 
     def __str__(self):
         return ", ".join(f"{a}={getattr(self, a)}" for a in self.kwargs)
@@ -38,6 +38,15 @@ class ReportingBase:
     def __repr__(self):
         text = str(self)
         return f"{self.__class__.__name__}({text})"
+
+    def __getitem__(self, key: str):
+        if self.has_attibute(key):
+            return getattr(self, key)
+        else:
+            return None
+
+    def has_attibute(self, attribute: str) -> bool:
+        return any(f.name == attribute for f in fields(self) if f.init)
 
     @property
     def report(self) -> dict:
@@ -55,11 +64,15 @@ class ReportingBase:
     def status_text(self) -> str:
         return self.status.value
 
+    @property
+    def kwargs(self) -> Iterator[str]:
+        return (f.name for f in fields(self) if f.init)
+
 
 if __name__ == "__main__":
     print(" START LOCAL TEST ".center(79, "="))
     r = ReportingBase(
-        city="cidade",
+        # city="cidade",
         message="Mensagem de teste.",
         status="success",
     )
