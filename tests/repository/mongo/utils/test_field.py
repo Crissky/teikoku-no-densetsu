7b6661f624d0e748ffa795aad4981b716dc9aaa4
Crissky <pg2006pe@hotmail.com>
2026-09-03 19:08:32 -0300

    Adiciona testes de QueryField

---
+++ tests/repository/mongo/utils/test_field.py
@@ -0,0 +1,35 @@
+import unittest
+from enum import Enum
+
+from repository.mongo.utils.field import QueryField
+
+
+class SampleEnum(Enum):
+    NAME = "sample_name"
+
+
+class TestQueryField(unittest.TestCase):
+
+    def test_plain_string_field(self):
+        qf = QueryField("name", "Alice")
+        self.assertEqual(qf.field, "name")
+
+    def test_enum_field_extracts_value(self):
+        qf = QueryField(SampleEnum.NAME, "Alice")
+        self.assertEqual(qf.field, "sample_name")
+
+    def test_no_type_check_accepts_any_value(self):
+        qf = QueryField("score", 42)
+        self.assertEqual(qf.value, 42)
+
+    def test_valid_type_check_passes(self):
+        qf = QueryField("score", 42, int)
+        self.assertEqual(qf.value, 42)
+
+    def test_invalid_type_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            QueryField("score", "not_an_int", int)
+
+    def test_query_property(self):
+        qf = QueryField("level", 5, int)
+        self.assertEqual(qf.query, {"level": 5})
