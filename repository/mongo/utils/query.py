f5f9edb053880aea5aa6f55bfa9edb4dd4fcf86f
Crissky <pg2006pe@hotmail.com>
2026-09-06 00:12:18 -0300

    Adiciona novos métodos Adiciona iter, str, repr e load_values

--- repository/mongo/utils/query.py
+++ repository/mongo/utils/query.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from repository.mongo.utils.field import QueryField
 
 
@@ -5,12 +7,40 @@ class Query:
     def __init__(self, *query_fields: QueryField):
         self.query_fields = query_fields
 
+    def __iter__(self):
+        return iter(self.query_fields)
+
+    def __str__(self):
+        text = ", ".join((str(qf) for qf in self))
+        return f"[{text}]"
+
+    def __repr__(self):
+        text = ", ".join((repr(qf) for qf in self))
+        return f"{self.__class__.__name__}([{text}])"
+
+    def load_values(self, obj: Any):
+        for qf in self.query_fields:
+            qf.load_value(obj)
+
     @property
     def query(self) -> dict:
         query = {}
         for qf in self.query_fields:
             if qf.field in query:
-                raise ValueError(f"Campo '{qf.field}' duplicado.")
+                raise ValueError(f"Campo {qf.field!r} duplicado.")
             query.update(qf.query)
 
         return query
+
+
+if __name__ == "__main__":
+    print(" START LOCAL TEST ".center(79, "="))
+
+    qf1 = QueryField("test", 123, int)
+    qf2 = QueryField("test2", "abc", str)
+    q = Query(qf1, qf2)
+    print("STR:", q)
+    print("REPR:", repr(q))
+    print("QUERY:", q.query)
+
+    print(" END LOCAL TEST ".center(79, "="))
--- tests/repository/mongo/utils/test_query.py
+++ tests/repository/mongo/utils/test_query.py
@@ -21,3 +21,36 @@ class TestQuery(unittest.TestCase):
     def test_duplicate_field_raises_value_error(self):
         with self.assertRaises(ValueError):
             Query(QueryField("name", "Alice"), QueryField("name", "Bob")).query
+
+    def test_iter_yields_query_fields(self):
+        qf1, qf2 = QueryField("a", 1), QueryField("b", 2)
+        q = Query(qf1, qf2)
+        self.assertEqual(list(q), [qf1, qf2])
+
+    def test_str_single_field(self):
+        q = Query(QueryField("x", 1))
+        self.assertEqual(str(q), "[{'x': 1}]")
+
+    def test_str_empty(self):
+        self.assertEqual(str(Query()), "[]")
+
+    def test_str_multiple_fields(self):
+        q = Query(QueryField("a", 1), QueryField("b", 2))
+        self.assertEqual(str(q), "[{'a': 1}, {'b': 2}]")
+
+    def test_repr_format(self):
+        q = Query(QueryField("x", 1))
+        self.assertEqual(repr(q), "Query([QueryField('x'=1 None)])")
+
+    def test_load_values_updates_fields_from_object(self):
+        class Obj:
+            name = "Bob"
+            age = 25
+        q = Query(QueryField("name", None), QueryField("age", None))
+        q.load_values(Obj())
+        self.assertEqual(q.query, {"name": "Bob", "age": 25})
+
+    def test_load_values_raises_attribute_error_for_missing_field(self):
+        q = Query(QueryField("missing", None))
+        with self.assertRaises(AttributeError):
+            q.load_values(object())
